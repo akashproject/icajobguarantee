197141babe2f5ea757c63969d847c9d30cc7cba4
PROLOY GHOSH <proloyghosh498@gmail.com>
2023-11-03 18:43:23 +0530

    Receive spelling change

--- eduversity/resources/views/page/refer-and-earn.blade.php
+++ eduversity/resources/views/page/refer-and-earn.blade.php
@@ -42,7 +42,7 @@
                                             <div class="lead_steps step2">
                                                 <div class="otp-content">
                                                     <h4 class="otp-heading"> OTP Verification </h4>                                   
-                                                    <p class="message"> <span class="message"> Enter the OTP you recive at </span> +91 XXXXXX<span class="lastDigit"></span> <span class="changeGivenNumber"> (Change) </span> </p>
+                                                    <p class="message"> <span class="message"> Enter the OTP you receive at </span> +91 XXXXXX<span class="lastDigit"></span> <span class="changeGivenNumber"> (Change) </span> </p>
                                                     <p class="response_status" style="color: #000;"></p>
                                                 </div> 
                                                 <div class="contact-info">
